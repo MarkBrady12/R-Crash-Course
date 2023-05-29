812fd214fc977d8c8a41709d6861a8c51d59ccf2
Mark Brady <bradym12@tcd.ie>
2023-05-29 13:47:07 +0100

    Classes

--- Crash_Course.R
+++ Crash_Course.R
@@ -20,4 +20,43 @@ starwars %>%
   #View()
   plot()
 
+View(msleep)
 
+glimpse(msleep)
+
+head(msleep)
+
+class(msleep$name)
+
+length(msleep)
+
+length(msleep$name)
+
+names(msleep)
+
+unique(msleep$vore)
+
+missing <- !complete.cases(msleep)
+
+msleep[missing, ]
+
+starwars %>% 
+  select(name, height, mass)
+
+starwars %>% 
+  select(ends_with("color")) %>% 
+  View()
+
+starwars %>%
+  rename("characters" = "name") %>% 
+  head()
+
+class(starwars$hair_color)
+
+starwars$hair_color <- as.factor(starwars$hair_color)
+
+class(starwars$hair_color)
+
+starwars %>% 
+  mutate(hair_color = as.character(hair_color)) %>% 
+  glimpse()
