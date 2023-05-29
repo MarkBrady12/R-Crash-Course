c041a0d68de705f93fdbd0906c9be4d22bad264e
Mark Brady <bradym12@tcd.ie>
2023-05-29 13:30:39 +0100

    Tidyverse Package
    Exploring the Tidyverse Package

--- Crash_Course.R
+++ Crash_Course.R
@@ -4,3 +4,20 @@ names <- c("John", "James")
 friends <- data.frame(names, ages)
 View(friends)
 str(friends)
+
+friends$ages
+friends$names
+
+data()
+View(starwars)
+library(tidyverse)
+
+starwars %>%
+  filter(height > 150 & mass < 200) %>%
+  mutate(height_in_meters = height/100) %>%
+  select(height_in_meters, mass) %>%
+  arrange(mass) %>%
+  #View()
+  plot()
+
+
